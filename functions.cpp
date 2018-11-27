dbab9ba76aeea9d93d93a345b3bc4e7b2b776553
Nikolas <nikolasbajen@gmail.com>
2018-11-27 13:57:34 +0100

    Changes

---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
+//#include "triangle.h"
+
+
+
+int main()
+{
+
+    
+   
+}
--- polygon.cpp
+++ polygon.cpp
@@ -8,17 +8,15 @@ int main()
     std::cout.setf(std::ios::fixed,std::ios::floatfield);
     std::cout.precision(3);
     int coordinates;
+    int halfCoordinates;
     int xBuffer = 0;
     int yBuffer = 1;
     float a = 0;
-    float *firstArray = new float[coordinates];
-    float *xCoord = new float[coordinates];
-    float *yCoord = new float[coordinates];
-    float *xValues = new float[coordinates];
-    float *yValues = new float[coordinates];
     float circumference;
     float positionX;
     float positionY;
+    float area;
+    float total;
 
     std::ifstream testFile;
     testFile.open("test.txt");
@@ -30,6 +28,13 @@ int main()
             coordinates += 1;
         }
     }
+    halfCoordinates = coordinates/2;
+    int j = (halfCoordinates - 1);
+    float *firstArray = new float[coordinates];
+    float *xValues = new float[halfCoordinates];
+    float *yValues = new float[halfCoordinates];
+    float *xCoord = new float[halfCoordinates];
+    float *yCoord = new float[halfCoordinates];
     testFile.close();
     testFile.open("test.txt");
         for(int i = 0; i < coordinates; i++)
@@ -37,7 +42,7 @@ int main()
         testFile >> firstArray[i];
         }
 
-            for(int i = 0; i < coordinates/2; i++)
+            for(int i = 0; i < halfCoordinates; i++)
             {
             xCoord[i] = firstArray[xBuffer];
             yCoord[i] = firstArray[yBuffer];
@@ -47,39 +52,29 @@ int main()
             }
             std::cout << std::endl;
 
-            for(int i = 0; i < coordinates/2; i++)
+            for(int i = 0; i < halfCoordinates; i++)
             {
-                xValues[i] += xCoord[i] * yCoord[i+1];
-                if(yCoord[i+1] <= 0)
-                {
-                    yCoord[i+1] = yCoord[i];
-                    xValues[i] += xCoord[i] * yCoord[0];
-                }
-                std::cout << xValues[i] << "\n";
+                total += (xCoord[j] + xCoord[i]) * (yCoord[j]-yCoord[i]);
+                j = i;
             }
-
-            for(int i = 0; i < coordinates/2; i++)
-            {
-                yValues[i] += yCoord[i] * xCoord[i+1];
-                if(xCoord[i+1] <= 0)
+                area = (total/2);
+                if(area < 0)
                 {
-                    xCoord[i+1] = xCoord[i];
-                    yValues[i] += yCoord[i] * xCoord[0];
+                    area = (abs(total/2));
                 }
-                std::cout << yValues[i] << "\n";
-            }
+                std::cout << "The area is: " << area << "\n";
 
-            for(int i = 0; i < coordinates/2; i++)
+            for(int i = 0; i < halfCoordinates; i++)
             {
                 positionX += xCoord[i];
             }
-                positionX /= (coordinates/2);
+                positionX /= halfCoordinates;
 
-            for(int i = 0; i < coordinates/2; i++)
+            for(int i = 0; i < halfCoordinates; i++)
             {
                 positionY += yCoord[i];
             }
-                positionY /= (coordinates/2);
+                positionY /= halfCoordinates;
 
                 std::cout << "The center position is (" << positionX << "," << positionY << ")" << "\n";
 
